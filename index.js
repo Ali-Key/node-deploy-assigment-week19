023221c67276f6808707bf59b7d632c176423a77
Ali-Key <mafiobio700@gmail.com>
2023-10-05 00:10:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Import server here and start the application
+
+import server from "./api/server.js";
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
